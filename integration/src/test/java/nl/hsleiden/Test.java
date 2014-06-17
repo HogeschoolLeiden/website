0e32709b36d8cb39d509eaaf9bc71221dc3426a1
Ebrahim Aharpour <ebrahim.aharpour@gmail.com>
2014-06-17 18:30:36 +0200

    Integration test was added.

---
+++ integration/src/test/java/nl/hsleiden/Test.java
@@ -0,0 +1,16 @@
+package nl.hsleiden;
+
+
+
+
+public class Test {
+
+	@org.junit.Test
+	public void test() {
+		System.out.println("**************************************");
+		System.out.println("**************************************");
+		System.out.println("**************************************");
+		System.out.println("**************************************");
+		System.out.println("**************************************");
+	}
+}
