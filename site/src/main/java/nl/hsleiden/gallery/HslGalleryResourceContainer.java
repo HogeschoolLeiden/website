3cf41886d55e328b5b4ba63f05cc9932daddcfd0
Gridi Serbo <gridi.serbo@gmail.com>
2014-08-06 12:56:19 +0200

    complete pinterest sharing on pretty photo

---
+++ site/src/main/java/nl/hsleiden/gallery/HslGalleryResourceContainer.java
@@ -0,0 +1,12 @@
+package nl.hsleiden.gallery;
+
+import org.hippoecm.hst.core.linking.AbstractResourceContainer;
+
+public class HslGalleryResourceContainer extends AbstractResourceContainer {
+
+	@Override
+	public String getNodeType() {
+		return "hsl:ImageSet";
+	}
+
+}
