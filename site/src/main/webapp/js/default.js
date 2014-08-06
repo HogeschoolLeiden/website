110fe7e02381d5c87e14c319dc4db05c645e438f
Gridi Serbo <gridi.serbo@gmail.com>
2014-08-06 10:39:53 +0200

    some changes in pretty photo for adding share button

--- site/src/main/webapp/js/default.js
+++ site/src/main/webapp/js/default.js
@@ -18,12 +18,18 @@ $(document).ready(function(){
 		deeplinking: true, /* Allow prettyPhoto to update the url to enable deeplinking. */
 		overlay_gallery: true, /* If set to true, a gallery will overlay the fullscreen image on mouse over */
 		keyboard_shortcuts: true, /* Set to false if you open forms inside prettyPhoto */
+		changepicturecallback: function(){
+            jQuery(".pp_content").css("height", $(".pp_content").height() + jQuery(".download-btn").outerHeight() - 23);
+        },
 		callback: function(){}, /* Called when prettyPhoto is closed */
 		ie6_fallback: true,
 		markup: '<div class="pp_pic_holder"> \
 					<div class="ppt" style="margin-bottom: -3px;">&nbsp;</div> \
 					<div class="pp_top"> \
-						<div class="pp_left"></div> \
+						<div class="pp_left">\
+							<div class="pp_social">\
+							</div>\
+						</div>\
 						<div class="pp_middle"></div> \
 						<div class="pp_right">\
 							<a class="pp_close" href="#">Close</a> \
@@ -73,17 +79,18 @@ $(document).ready(function(){
 							</div> \
 							<a href="#" class="pp_arrow_next">Next</a> \
 						</div>',
-		image_markup: '<img id="fullResImage" src="{path}" />',
+		image_markup: '<img id="fullResImage" src="{path}" /><span class="download-btn"><a href="{path}?forceDownload=true" title="Download">Download</a></span>',
 		flash_markup: '<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" width="{width}" height="{height}"><param name="wmode" value="{wmode}" /><param name="allowfullscreen" value="true" /><param name="allowscriptaccess" value="always" /><param name="movie" value="{path}" /><embed src="{path}" type="application/x-shockwave-flash" allowfullscreen="true" allowscriptaccess="always" width="{width}" height="{height}" wmode="{wmode}"></embed></object>',
 		quicktime_markup: '<object classid="clsid:02BF25D5-8C17-4B23-BC80-D3488ABDDC6B" codebase="http://www.apple.com/qtactivex/qtplugin.cab" height="{height}" width="{width}"><param name="src" value="{path}"><param name="autoplay" value="{autoplay}"><param name="type" value="video/quicktime"><embed src="{path}" height="{height}" width="{width}" autoplay="{autoplay}" type="video/quicktime" pluginspage="http://www.apple.com/quicktime/download/"></embed></object>',
 		iframe_markup: '<iframe src ="{path}" width="{width}" height="{height}" frameborder="no"></iframe>',
 		inline_markup: '<div class="pp_inline">{content}</div>',
 		custom_markup: '',
-		image_markup: '<img id="fullResImage" src="{path}" /><span class="download-btn"><a href="{path}?forceDownload=true" title="Download">Download</a></span>',
-        changepicturecallback: function(){
-            jQuery(".pp_content").css("height", $(".pp_content").height() + jQuery(".download-btn").outerHeight() - 23);
-        },
-		//social_tools: '<div class="pp_social"><div class="pinterest"><a href="http://pinterest.com/pin/create/button/" class="pinterest-share-button" data-count="none">Pin</a><script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script></div> <div class="facebook"><iframe src="http://www.facebook.com/plugins/like.php?locale=en_US&href='+location.href+'&amp;layout=button_count&amp;show_faces=true&amp;width=500&amp;action=like&amp;font&amp;colorscheme=light&amp;height=23" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:500px; height:23px;" allowTransparency="true"></iframe></div></div>' /* html or false to disable */
-		social_tools: 'html' /* html or false to disable */
+        social_tools: '<div class="pp_social">\
+        	<div class="twitter">\
+        	<a href="http://twitter.com/share" class="twitter-share-button" data-count="none">\
+        	Tweet</a>\
+        	<script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>\
+        	</div>\
+        </div>'
 	});
 });
