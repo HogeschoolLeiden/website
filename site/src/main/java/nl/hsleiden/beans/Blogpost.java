3ddb8f7f1ef3d54e8db7e3bbe2d5df622d611724
Gridi Serbo <gridi.serbo@gmail.com>
2014-08-13 14:47:13 +0200

    add essentials hippo blog

---
+++ site/src/main/java/nl/hsleiden/beans/Blogpost.java
@@ -0,0 +1,98 @@
+package nl.hsleiden.beans;
+/*
+ * Copyright 2014 Hippo B.V. (http://www.onehippo.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import java.util.Calendar;
+import java.util.List;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+import org.hippoecm.hst.content.beans.standard.HippoHtml;
+import org.onehippo.cms7.essentials.components.model.Authors;
+import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerated;
+
+@HippoEssentialsGenerated(internalName = "hsl:blogpost")
+@Node(jcrType = "hsl:blogpost")
+public class Blogpost extends HippoDocument implements Authors {
+
+    public static final String TITLE = "hsl:title";
+    public static final String INTRODUCTION = "hsl:introduction";
+    public static final String CONTENT = "hsl:content";
+    public static final String PUBLICATION_DATE = "hsl:publicationdate";
+    public static final String CATEGORIES = "hsl:categories";
+    public static final String AUTHOR = "hsl:author";
+    public static final String AUTHOR_NAMES = "hsl:authornames";
+    public static final String LINK = "hsl:link";
+    public static final String AUTHORS = "hsl:authors";
+    public static final String TAGS = "hippostd:tags";
+
+
+   @HippoEssentialsGenerated(internalName = "hsl:publicationdate")
+    public Calendar getPublicationDate() {
+        return getProperty(PUBLICATION_DATE);
+    }
+
+    @HippoEssentialsGenerated(internalName = "hsl:authornames")
+    public String[] getAuthorNames() {
+        return getProperty(AUTHOR_NAMES);
+    }
+
+    public String getAuthor() {
+        final String[] authorNames = getAuthorNames();
+        if(authorNames !=null && authorNames.length > 0){
+            return authorNames[0];
+        }
+        return null;
+    }
+
+
+    @HippoEssentialsGenerated(internalName = "hsl:title")
+    public String getTitle() {
+        return getProperty(TITLE);
+    }
+
+    @HippoEssentialsGenerated(internalName = "hsl:content")
+    public HippoHtml getContent() {
+        return getHippoHtml(CONTENT);
+    }
+
+    @HippoEssentialsGenerated(internalName = "hsl:introduction")
+    public String getIntroduction() {
+        return getProperty(INTRODUCTION);
+    }
+
+
+    @HippoEssentialsGenerated(internalName = "hsl:link")
+    public String getLink() {
+        return getProperty(LINK);
+    }
+
+    @HippoEssentialsGenerated(internalName = "hsl:categories")
+    public List<String> getCategories() {
+        return getProperty(CATEGORIES);
+    }
+
+    @Override
+    @HippoEssentialsGenerated(internalName = "hsl:authors")
+    public List<Author> getAuthors() {
+        return getLinkedBeans(AUTHORS, Author.class);
+    }
+
+    @HippoEssentialsGenerated(internalName = "hippostd:tags")
+    public String[] getTags() {
+        return getProperty(TAGS);
+    }
+
+}
---
+++ site/src/main/webapp/WEB-INF/jsp/essentials/blog/essentials-blog-author-post.jsp
@@ -0,0 +1,42 @@
+<%@ include file="/WEB-INF/jsp/include/imports.jsp" %>
+<%--
+  Copyright 2014 Hippo B.V. (http://www.onehippo.com)
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+  --%>
+
+<%--@elvariable id="item" type="nl.hsleiden.beans.Blogpost"--%>
+<%--@elvariable id="author" type="nl.hsleiden.beans.Author"--%>
+<%--@elvariable id="pageable" type="org.onehippo.cms7.essentials.components.paging.Pageable"--%>
+<div class="panel panel-default">
+  <c:if test="${(pageable ne null)}">
+    <div class="panel-heading">
+      <h3 class="panel-title">More by ${author.fullName}</h3>
+    </div>
+    <c:choose>
+      <c:when test="${pageable.total gt 0}">
+        <div class="panel-body">
+          <c:forEach var="item" items="${pageable.items}" varStatus="status">
+            <hst:link var="link" hippobean="${item}"/>
+            <p><a href="${link}"><c:out value="${item.title}"/></a></p>
+          </c:forEach>
+        </div>
+      </c:when>
+      <c:otherwise>
+        <div class="panel-body">
+          <p>No other posts found.</p>
+        </div>
+      </c:otherwise>
+    </c:choose>
+  </c:if>
+</div>
