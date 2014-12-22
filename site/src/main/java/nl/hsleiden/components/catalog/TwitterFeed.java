3471713b9c9ed4a8413f07dbee39cc1109f2071b
Ebrahim Aharpour <ebrahim@open-web.nl>
2014-12-22 15:28:58 +0100

    TwitterFeed updated.

--- site/src/main/java/nl/hsleiden/components/catalog/TwitterFeed.java
+++ site/src/main/java/nl/hsleiden/components/catalog/TwitterFeed.java
@@ -25,6 +25,7 @@ import org.slf4j.LoggerFactory;
 
 import twitter4j.Query;
 import twitter4j.QueryResult;
+import twitter4j.ResponseList;
 import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
@@ -37,6 +38,7 @@ import com.tdclighthouse.prototype.utils.BeanUtils;
 public class TwitterFeed extends AjaxEnabledComponent {
 
     private static final Logger LOG = LoggerFactory.getLogger(TwitterFeed.class);
+    private Twitter twitter = TwitterFactory.getSingleton();
 
     @Override
     public Map<String, Object> getModel(HstRequest request, HstResponse response) {
@@ -48,59 +50,57 @@ public class TwitterFeed extends AjaxEnabledComponent {
 
             String from = parametersInfo.getFrom();
             String searchQuery = parametersInfo.getQuery();
-            String completeQuery = "";
-
-            if (from != null && !from.isEmpty()) {
-                completeQuery += "from:" + from + " ";
-            }
-            if (searchQuery != null && !searchQuery.isEmpty()) {
-                completeQuery += searchQuery;
-            }
 
             if (parametersInfo.getLimit() != 0) {
-                model.put("tweets", getTweets(completeQuery, parametersInfo, request));
+
+                if (from != null && !from.isEmpty()) {
+                    ResponseList<Status> userTimeline = twitter.timelines().getUserTimeline(from);
+                    model.put("tweets", populateTweetsList(userTimeline, parametersInfo));
+                } else if (searchQuery != null && !searchQuery.isEmpty()) {
+                    model.put("tweets", getTweetsByQuery(searchQuery, parametersInfo, request));
+                }
             } else {
                 request.setAttribute(WidgetConstants.WEB_MASTER_MESSAGE, "webmaster.0.tweets.message");
             }
 
             return model;
         } catch (RepositoryException e) {
-            LOG.error(e.getMessage(), e);
             throw new HstComponentException(e.getMessage(), e);
+        } catch (TwitterException e) {
+            throw new HstComponentException("Error while retrieving Tweets.", e);
         }
     }
 
-    private List<TweetStatus> getTweets(String completeQuery, TwitterFeedInfo info, HstRequest request) {
+    private List<TweetStatus> getTweetsByQuery(String completeQuery, TwitterFeedInfo info, HstRequest request)
+            throws TwitterException {
 
-        List<TweetStatus> result = new ArrayList<TweetStatus>();
+        List<TweetStatus> result = null;
 
         if (!completeQuery.isEmpty()) {
-            Twitter twitter = TwitterFactory.getSingleton();
             Query query = new Query(completeQuery);
             LOG.debug("Twitter feed query: " + completeQuery);
             query.setCount(info.getLimit());
             QueryResult queryResult = null;
-            try {
-                queryResult = twitter.search(query);
-                List<Status> statuses = queryResult.getTweets();
-                populateTweetsList(result, statuses, info);
+            queryResult = twitter.search(query);
+            List<Status> statuses = queryResult.getTweets();
+            result = populateTweetsList(statuses, info);
 
-            } catch (TwitterException e) {
-                LOG.error("Error while retrieving Tweets.", e);
-            }
         } else {
             request.setAttribute(WidgetConstants.WEB_MASTER_MESSAGE, "webmaster.notweets.message");
         }
         return result;
     }
 
-    private void populateTweetsList(List<TweetStatus> result, List<Status> statuses, TwitterFeedInfo info) {
-        for (Status status : statuses) {
+    private List<TweetStatus> populateTweetsList(List<Status> statuses, TwitterFeedInfo info) {
+        List<TweetStatus> result = new ArrayList<TweetStatus>();
+        for (int i = 0; i < statuses.size() && result.size() < info.getLimit(); i++) {
             TweetStatus ts = new TweetStatus();
+            Status status = statuses.get(i);
             ts.setStatus(status);
             ts.setText(TwitterUtils.convertMessage(status.getText(), status, info.getShowImages()));
             result.add(ts);
         }
+        return result;
     }
 
     private TwitterFeedInfo getConfiguration(HstRequest request) throws RepositoryException {
