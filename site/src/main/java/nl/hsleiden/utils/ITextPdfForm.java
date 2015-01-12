0febd55bd4ffb189eaf3dfef9356bd50b1ba809b
gridi <gridi.serbo@gmail.com>
2015-01-12 13:04:14 +0100

    fix for radio buttons handling

--- site/src/main/java/nl/hsleiden/utils/ITextPdfForm.java
+++ site/src/main/java/nl/hsleiden/utils/ITextPdfForm.java
@@ -31,7 +31,7 @@ import com.onehippo.cms7.eforms.hst.model.Form;
 public class ITextPdfForm {
 
     private static final Logger LOG = LoggerFactory.getLogger(ITextPdfForm.class);
-    
+
     public InputStream createFormPdf(FormBean formBean, Form form, String introText) {
 
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
@@ -41,7 +41,7 @@ public class ITextPdfForm {
             PdfWriter.getInstance(doc, baos);
 
             doc.open();
-            
+
             addTitle(formBean, doc);
             doc.add(Chunk.NEWLINE);
 
@@ -65,12 +65,27 @@ public class ITextPdfForm {
         PdfPTable tbl = new PdfPTable(2);
         for (AbstractField abstractField : fields) {
             if (abstractField.isMultiple()) {
-                addMultivalueFields(tbl, abstractField);
+                if ("radiogroup".equalsIgnoreCase(abstractField.getType())) {
+                    addSingleValueFields(tbl, abstractField);
+                } else {
+                    addMultivalueFields(tbl, abstractField);
+                }
             } else {
                 addSingleValueFields(tbl, abstractField);
             }
         }
         doc.add(tbl);
+    }
+
+    private void addMultivalueFields(PdfPTable tbl, AbstractField abstractField) {
+        Map<String, Collection<String>> multiValues = abstractField.getMultiValues();
+        for (Entry<String, Collection<String>> multivaluesEntry : multiValues.entrySet()) {
+            if (multivaluesEntry.getValue().toString() != null && !multivaluesEntry.getValue().isEmpty()
+                    && !multivaluesEntry.getValue().toString().isEmpty()) {
+                addTableCell(tbl, multivaluesEntry.getKey());
+                addTableCell(tbl, multivaluesEntry.getValue().toString());
+            }
+        }
     }
 
     private void addSingleValueFields(PdfPTable tbl, AbstractField abstractField) {
@@ -80,30 +95,13 @@ public class ITextPdfForm {
         }
     }
 
-    private void addMultivalueFields(PdfPTable tbl, AbstractField abstractField) {
-        Map<String, Collection<String>> multiValues = abstractField.getMultiValues();
-        for (Entry<String, Collection<String>> multivaluesEntry : multiValues.entrySet()) {
-            if (multivaluesEntry.getValue().toString() != null && !multivaluesEntry.getValue().isEmpty()
-                    && isRadioValueNotEmpty(abstractField)) {
-                addTableCell(tbl, multivaluesEntry.getKey());
-                addTableCell(tbl, multivaluesEntry.getValue().toString());
-            }
-        }
-    }
-
-    private boolean isRadioValueNotEmpty(AbstractField abstractField) {
-        return "radiogroup".equalsIgnoreCase(abstractField.getType()) && abstractField.getValue() != null
-                && !abstractField.getValue().isEmpty();
-    }
-
     private void addTableCell(PdfPTable tbl, String content) {
         PdfPCell cell = new PdfPCell(new Phrase(content));
         cell.disableBorderSide(Rectangle.BOX);
         tbl.addCell(cell);
     }
 
-    private void addIntroduction(FormBean formBean, String introText, Document doc)
-            throws DocumentException {
+    private void addIntroduction(FormBean formBean, String introText, Document doc) throws DocumentException {
         if (introText != null && !introText.isEmpty()) {
             doc.add(new Paragraph(introText));
         }
