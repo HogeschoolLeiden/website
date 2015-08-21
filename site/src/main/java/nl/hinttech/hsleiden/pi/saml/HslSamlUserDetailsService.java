d0c2a77e1b9d9b3c8c57997faf2cb822ac4ebb70
Ebrahim aharpour <ebrahim@openweb.nl>
2015-08-21 15:46:32 +0200

    Making sonar happy.

--- site/src/main/java/nl/hinttech/hsleiden/pi/saml/HslSamlUserDetailsService.java
+++ site/src/main/java/nl/hinttech/hsleiden/pi/saml/HslSamlUserDetailsService.java
@@ -12,54 +12,63 @@ import org.slf4j.LoggerFactory;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.saml.SAMLCredential;
 import org.springframework.security.saml.userdetails.SAMLUserDetailsService;
 
 public class HslSamlUserDetailsService implements SAMLUserDetailsService {
-    
+
     private static final Logger LOG = LoggerFactory.getLogger(HslSamlUserDetailsService.class);
-    
-    
-    public static final String USERNAME_FIELD = "urn:mace:dir:attribute-def:cn";
+
+    public static final String USERNAME_FIELD_2_0 = "urn:mace:dir:attribute-def:cn";
+    private static final String USERNAME_FIELD_1_1 = "urn:oid:2.5.4.3";
 
     @Override
-    public Object loadUserBySAML(SAMLCredential credential) throws UsernameNotFoundException {
+    public Object loadUserBySAML(SAMLCredential credential) {
         String userId = getUserId(credential);
         return new User(userId, userId, getAuthorities(credential));
     }
-    
-    public static String getUserId(SAMLCredential credential) {
-        final List<String> userIdValueList = getAttributeValue("urn:mace:dir:attribute-def:cn", credential);
+
+    public String getUserId(SAMLCredential credential) {
+        final List<String> userIdValueList = getAttributeValue(USERNAME_FIELD_2_0, USERNAME_FIELD_1_1, credential);
         String userId = null;
-        if (userIdValueList.size() > 0) {
+        if (!userIdValueList.isEmpty()) {
             userId = userIdValueList.get(0);
         }
         return userId;
     }
-    
-    public static List<String> getAttributeValue(final String name, final SAMLCredential credential) {
-        List<String> attrValList = new ArrayList<String>();
-        Attribute attribute = credential.getAttribute(name);
-        LOG.info("Parsing SAML attribute name:  {}", name);
+
+    public List<String> getAttributeValue(String attributeName, String fallbackAttributeName,
+            final SAMLCredential credential) {
+        List<String> attrValList;
+        Attribute attribute = credential.getAttribute(attributeName);
+        LOG.info("Parsing SAML attribute name:  {}", attributeName);
         if (attribute != null) {
-            List<XMLObject> attributes = attribute.getAttributeValues();
-            if ((attributes != null) && (attributes.size() > 0)) {
-                for (XMLObject object : attributes) {
-                    XSString attrb = (XSString) object;
-                    String attrValue = attrb.getValue();
-                    if (attrValue != null) {
-                        LOG.info("attrValue {}", attrValue);
-                        attrValList.add(attrValue);
-                    }
+            attrValList = getAttributeValues(attribute);
+        } else {
+            attribute = credential.getAttribute(fallbackAttributeName);
+            attrValList = attribute != null ? getAttributeValues(attribute) : new ArrayList<String>();
+        }
+        return attrValList;
+    }
+
+    private List<String> getAttributeValues(Attribute attribute) {
+        List<String> attrValList = new ArrayList<String>();
+        List<XMLObject> attributes = attribute.getAttributeValues();
+        if (attributes != null && !attributes.isEmpty()) {
+            for (XMLObject object : attributes) {
+                XSString attrb = (XSString) object;
+                String attrValue = attrb.getValue();
+                if (attrValue != null) {
+                    LOG.info("attrValue {}", attrValue);
+                    attrValList.add(attrValue);
                 }
             }
         }
         return attrValList;
     }
 
-    private static Collection<? extends GrantedAuthority> getAuthorities(SAMLCredential credential) {
-        ArrayList<GrantedAuthority> roles = new ArrayList<GrantedAuthority>();
+    private Collection<GrantedAuthority> getAuthorities(SAMLCredential credential) {
+        List<GrantedAuthority> roles = new ArrayList<GrantedAuthority>();
         roles.add(new SimpleGrantedAuthority("ROLE_ANONYMOUS"));
         Attribute attribute = credential.getAttribute("urn:mace:dir:attribute-def:eduPersonAffiliation");
         if (attribute == null) {
